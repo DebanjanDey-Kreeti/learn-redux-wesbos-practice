38ae332b7bc8213877238b36b18f2a6d2cd928c5
DebanjanDey-Kreeti <debanjan.dey@kreeti.com>
2019-07-18 15:06:56 +0530

    Commit 1

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const PhotoGrid = React.createClass({
+  render() {
+    return (
+      <div>
+        <div className="single-photo">
+          Main ek photo grid hoo!
+        </div>
+      </div>
+    )
+  }
+});
+
+export default PhotoGrid;
